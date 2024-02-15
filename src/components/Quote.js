3c19eaee0109bcd5d2bd8ac693be5d68da086de8
Nkululeko1999 <105264276+Nkululeko1999@users.noreply.github.com>
2024-02-15 14:46:27 +0200

    Update Quote.js, toast notifications

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { toast } from "react-toastify";
 
 export default function Quote() {
   const [formData, setFormData] = useState({
@@ -21,6 +22,7 @@ export default function Quote() {
     try {
       
       const response = await axios.post("https://electricians-api.onrender.com/api/auto-responder/quote", formData);
+      toast.success(response.data.message);
       
       console.log('Success:', response.data);
     } catch (error) {
@@ -28,6 +30,7 @@ export default function Quote() {
       if (error.response) {
 
         console.error('Server responded with error data:', error.response.data);
+        toast.error(error.response.data.message);
         console.error('Status code:', error.response.status);
         console.error('Headers:', error.response.headers);
       } else if (error.request) {
