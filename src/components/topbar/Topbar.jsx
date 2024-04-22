eb388618794266a73e09164cb8778272e5574c1e
Nkululeko <n.mkhatywa2019@gmail.com>
2024-04-22 13:04:27 +0200

    location when topbar hovered

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,15 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+
 
 function Topbar() {
+
+    const [isHovered, setIsHovered] = useState(false);
+
   return (
     <div className="container-fluid px-5 d-none d-lg-block topbar">
         <div className="row gx-0 align-items-center" style={{height: "45px"}}>
             <div className="col-lg-6 text-center text-lg-start mb-lg-0">
-                <div className="d-flex justify-content-start items-center flex-wrap">
-                    <div className='text-white topbar-box'>
+                <div className="d-flex justify-content-start items-center flex-wrap relative">
+                    <div className='text-white topbar-box'
+                    onMouseEnter={() => setIsHovered(true)}
+                    onMouseLeave={() => setIsHovered(false)}
+                    >
                         <i class="bi bi-geo-alt-fill topbar-icons"></i>
                         <span>Our Location</span>
                     </div>
+                    {isHovered && <div className='px-3 py-4 location rounded'>
+                            <span>8832 Shukuma Street</span> <br />
+                            <span>Midrand, Johannesburg</span> <br />
+                            <span>1685</span>
+                        </div>
+                        }
                     <div className='text-white topbar-box'>
                         <i class="bi bi-telephone-forward-fill topbar-icons"></i>
                         <span>078 937 6656 / 063 571 7049</span>
